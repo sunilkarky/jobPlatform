a87887993eb53751332c0b57d2ef3f6df58aa6b5
sunilkarky <sunilkarki670@gmail.com>
2025-02-05 12:02:03 +0545

    landing page error fix

--- backend/app.js
+++ backend/app.js
@@ -26,10 +26,6 @@ app.listen(3000, () => {
   console.log("Started running at port 3000");
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
 app.use("/", userRoute);
 app.use("/", tuitionRoute);
 
