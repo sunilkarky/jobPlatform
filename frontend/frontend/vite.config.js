b0c4333fafbdae5c5b622232cab0b3c0e9ad9909
sunilkarky <sunilkarki670@gmail.com>
2025-02-05 13:49:29 +0545

    route add admin

--- frontend/frontend/vite.config.js
+++ frontend/frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import rewriteAll from "vite-plugin-rewrite-all";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), rewriteAll()],
+  build: {
+    outDir: "build",
+  },
+});
